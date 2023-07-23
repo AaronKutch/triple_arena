fd4044bf87e6e0db355062404954ab7313b127bd
Aaron Kutch <aaronkutch@att.net>
2023-07-23 02:22:55 -0500

    Update ord_iterators.rs

--- triple_arena/src/ord_iterators.rs
+++ triple_arena/src/ord_iterators.rs
@@ -1,18 +1,35 @@
 //! Iterators for `OrdArena`
 
-use crate::{OrdArena, Ptr};
+use crate::{Link, OrdArena, Ptr};
 
-/// All the iterators here can return values in arbitrary order
+/// An iterator over the valid `P`s of an `OrdArena`
+pub struct Ptrs<'a, P: Ptr, K: Ord, V> {
+    arena: &'a OrdArena<P, K, V>,
+    p: Option<P>,
+}
+
+impl<'a, P: Ptr, K: Ord, V> Iterator for Ptrs<'a, P, K, V> {
+    type Item = P;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let p_res = self.p;
+        self.arena.next_ptr(&mut self.p);
+        p_res
+    }
+}
+
+/// All the iterators here iterate in order from the least key to the greatest
+/// key
 impl<P: Ptr, K: Ord, V> OrdArena<P, K, V> {
-    // TODO make all iterators go in order
-    /*
     /// Iteration over all valid `P` in the arena
-    pub fn ptrs(&self) -> Ptrs<P, K> {
+    pub fn ptrs(&self) -> Ptrs<P, K, V> {
         Ptrs {
-            iter: self.a.ptrs(),
+            arena: self,
+            p: self.min(),
         }
     }
 
+    /*
     /// Iteration over `&K`
     pub fn keys(&self) -> Keys<P, K> {
         Keys {
@@ -50,4 +67,32 @@ impl<P: Ptr, K: Ord, V> OrdArena<P, K, V> {
             vals: &self.vals,
         }
     }*/
+
+    /// Similar to [crate::ChainArena::next_chain_ptr] except it iterates over
+    /// pointers in order of the keys. In order to iterate over all pointers:
+    ///
+    /// ```text
+    /// let mut p = arena.min();
+    /// loop {
+    ///     if let Some(p) = p {
+    ///         // use `p` here, but be aware that the removal or insertion of
+    ///         // elements within this loop is not supported
+    ///     } else {
+    ///         break
+    ///     }
+    ///     arena.next_ptr(&mut p);
+    /// }
+    /// ```
+    #[inline]
+    pub fn next_ptr(&self, p: &mut Option<P>) {
+        if let Some(inner) = p {
+            if let Some(link) = self.a.get(*inner) {
+                if let Some(next) = Link::next(link) {
+                    *p = Some(next);
+                }
+            }
+            *p = None;
+        }
+        // else `p` is already `None`
+    }
 }
