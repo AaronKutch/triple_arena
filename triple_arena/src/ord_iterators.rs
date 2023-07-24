c478cb735ee8ca58546871548231762374014a27
Aaron Kutch <aaronkutch@att.net>
2023-07-23 23:07:25 -0500

    Update ord_iterators.rs

--- triple_arena/src/ord_iterators.rs
+++ triple_arena/src/ord_iterators.rs
@@ -18,6 +18,75 @@ impl<'a, P: Ptr, K: Ord, V> Iterator for Ptrs<'a, P, K, V> {
     }
 }
 
+/// An iterator over `&K` in an `OrdArena`
+pub struct Keys<'a, P: Ptr, K: Ord, V> {
+    arena: &'a OrdArena<P, K, V>,
+    p: Option<P>,
+}
+
+impl<'a, P: Ptr, K: Ord, V> Iterator for Keys<'a, P, K, V> {
+    type Item = &'a K;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let p_res = self.p;
+        self.arena.next_ptr(&mut self.p);
+        p_res.map(|p| self.arena.get_key(p).unwrap())
+    }
+}
+
+/// An iterator over `&V` in an `OrdArena`
+pub struct Vals<'a, P: Ptr, K: Ord, V> {
+    arena: &'a OrdArena<P, K, V>,
+    p: Option<P>,
+}
+
+impl<'a, P: Ptr, K: Ord, V> Iterator for Vals<'a, P, K, V> {
+    type Item = &'a V;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let p_res = self.p;
+        self.arena.next_ptr(&mut self.p);
+        p_res.map(|p| self.arena.get_val(p).unwrap())
+    }
+}
+
+/*
+/// A mutable iterator over `&mut V` in an `OrdArena`
+pub struct ValsMut<'a, P: Ptr, K: Ord, V> {
+    arena: &'a mut OrdArena<P, K, V>,
+    p: Option<P>,
+}
+
+impl<'a, P: Ptr, K: Ord, V> Iterator for ValsMut<'a, P, K, V> {
+    type Item = &'a mut V;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let p_res = self.p;
+        self.arena.next_ptr(&mut self.p);
+        p_res.map(|p| self.arena.get_val_mut(p).unwrap())
+    }
+}
+*/
+
+/// An iterator over `(P, &K, &V)` in an `OrdArena`
+pub struct Iter<'a, P: Ptr, K: Ord, V> {
+    arena: &'a OrdArena<P, K, V>,
+    p: Option<P>,
+}
+
+impl<'a, P: Ptr, K: Ord, V> Iterator for Iter<'a, P, K, V> {
+    type Item = (P, &'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let p_res = self.p;
+        self.arena.next_ptr(&mut self.p);
+        p_res.map(|p| {
+            let tmp = self.arena.get(p).unwrap();
+            (p, tmp.0, tmp.1)
+        })
+    }
+}
+
 /// All the iterators here iterate in order from the least key to the greatest
 /// key
 impl<P: Ptr, K: Ord, V> OrdArena<P, K, V> {
@@ -29,44 +98,38 @@ impl<P: Ptr, K: Ord, V> OrdArena<P, K, V> {
         }
     }
 
-    /*
     /// Iteration over `&K`
-    pub fn keys(&self) -> Keys<P, K> {
+    pub fn keys(&self) -> Keys<P, K, V> {
         Keys {
-            iter: self.keys.vals(),
+            arena: self,
+            p: self.min(),
         }
     }
 
     /// Iteration over `&V`
-    pub fn vals(&self) -> Vals<P, V> {
+    pub fn vals(&self) -> Vals<P, K, V> {
         Vals {
-            iter: self.vals.vals(),
+            arena: self,
+            p: self.min(),
         }
     }
 
-    /// Mutable iteration over `&mut K`
-    pub fn keys_mut(&mut self) -> KeysMut<P, K> {
-        KeysMut {
-            iter_mut: self.keys.vals_mut(),
-        }
-    }
-
+    /*
     /// Mutable iteration over `&mut V`
     pub fn vals_mut(&mut self) -> ValsMut<P, V> {
         ValsMut {
             iter_mut: self.vals.vals_mut(),
         }
     }
+    */
 
-    /// Iteration over `(P, &K, &V)` tuples. For each surject with multiple `P`
-    /// pointing to the same `V`, the same reference to the `V` is returned
-    /// multiple times
+    /// Iteration over `(P, &K, &V)` tuples
     pub fn iter(&self) -> Iter<P, K, V> {
         Iter {
-            iter: self.keys.iter(),
-            vals: &self.vals,
+            arena: self,
+            p: self.min(),
         }
-    }*/
+    }
 
     /// Similar to [crate::ChainArena::next_chain_ptr] except it iterates over
     /// pointers in order of the keys. In order to iterate over all pointers:
