e321efa32bedc3bf17882e17f81d66aca405e976
AaronKutch <aaronkutch@att.net>
2026-08-14 17:53:39 -0500

    aei

--- triple_arena/src/ord.rs
+++ triple_arena/src/ord.rs
@@ -1,11 +1,11 @@
 mod find;
 mod impl_ord_arena_trait;
 mod insert;
-mod ord_arena;
 mod ord_arena_trait;
 pub mod ord_iterators;
 mod remove;
+mod simple_ord_arena;
 
 pub use insert::{OrdInsertKind, SimpleOrdArenaInsertEntry};
-pub use ord_arena::{SimpleOrdArena, SimpleOrdArenaNode};
 pub use ord_arena_trait::{OrdPair, SimpleOrdItem};
+pub use simple_ord_arena::{SimpleOrdArena, SimpleOrdArenaNode};
