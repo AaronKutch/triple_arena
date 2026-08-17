b3bca767a8c25d2d15f5e1c71c9360c0d6e9cab3
AaronKutch <aaronkutch@att.net>
2026-08-17 13:40:21 -0500

    notes

--- testcrate/src/simple_ord_arena.rs
+++ testcrate/src/simple_ord_arena.rs
@@ -1,1 +1,30 @@
 // FIXME
+/*
+
+    // avoid getting mixups
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    struct Key {
+        pub k: u8,
+    }
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    struct Triple<P> {
+        pub k: Key,
+        pub v: Ck<()>,
+        pub p: P,
+    }
+
+impl SimpleOrdItem for Triple
+
+    // make sure we have collisions
+    const KEY_LIMIT: u8 = 64;
+    let mut new_k = || Key {
+        k: key_rng.index(KEY_LIMIT).unwrap(),
+    };
+
+
+    let mut a = SimpleOrdArena::<P, Triple<P>>::new();
+
+    // this handles nonhereditary cases
+    let mut b: BTreeMap<Key, BTreeMap<Ck<()>, Triple<P>>> = BTreeMap::new();
+*/
--- testcrate/src/surject_arena.rs
+++ testcrate/src/surject_arena.rs
@@ -1,1 +1,23 @@
 // FIXME
+/*
+
+    struct Entry {
+        pub v: Val,
+        pub p: P,
+    }
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    struct Val {
+        pub v: u64,
+    }
+
+    let mut counter_v = 0u64;
+    let mut new_v = || {
+        counter_v += 1;
+        Val { v: counter_v }
+    };
+
+    let mut a = SurjectArena::<P, Ck<()>, Val>::new();
+    let mut b_entries = CkMap::<(), Entry>;
+    let mut b_sets = HashMap<Val, Vec<Ck<()>>>;
+*/
